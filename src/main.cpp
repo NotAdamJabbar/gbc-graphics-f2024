f849934732b763700a960f47c5d83baf98876af8
NotAdamJabbar <147456041+NotAdamJabbar@users.noreply.github.com>
2024-09-16 16:03:02 -0400

    s

--- src/main.cpp
+++ src/main.cpp
@@ -108,7 +108,7 @@ int main(void)
         case 1:
             glUseProgram(shaderUniformColor);
             glUniformMatrix4fv(u_world, 1, GL_FALSE, ToFloat16(world).v);
-            glUniform3f(u_color, 1.0f, 0.0f, 0.0f);
+            glUniform3f(u_color, 1.0f, 1.0f, 1.0f);
             glUniform1f(u_intensity, 1.0f);
             glDrawArrays(GL_TRIANGLES, 0, 3);
             break;
@@ -116,10 +116,10 @@ int main(void)
         // Hint: Switch the shader to colour based on vertex positions
         // If you get errors in the console, comment out all unused uniforms
         case 2:
-            glUseProgram(shaderUniformColor);
-            glUniformMatrix4fv(u_world, 1, GL_FALSE, ToFloat16(world).v);
-            glUniform3f(u_color, 0.0f, 1.0f, 0.0f);
-            glUniform1f(u_intensity, 1.0f);
+            glUseProgram(shaderVertexColor);
+            //glUniformMatrix4fv(u_world, 1, GL_FALSE, ToFloat16(world).v);
+            //glUniform3f(u_color, 0.0f, 1.0f, 0.0f);
+            //glUniform1f(u_intensity, 1.0f);
             glDrawArrays(GL_TRIANGLES, 0, 3);
             break;
 
@@ -128,12 +128,13 @@ int main(void)
             glUseProgram(shaderUniformColor);
             glUniformMatrix4fv(u_world, 1, GL_FALSE, ToFloat16(world).v);
             glUniform3f(u_color, 0.0f, 0.0f, 1.0f);
-            glUniform1f(u_intensity, 1.0f);
+            glUniform1f(u_intensity, sin(time)*0.5f+0.5f);
             glDrawArrays(GL_TRIANGLES, 0, 3);
             break;
 
         // Hint: Use the Translate function
         case 4:
+            world = Translate(sin(time), 0.0f, 0.0f);
             glUseProgram(shaderUniformColor);
             glUniformMatrix4fv(u_world, 1, GL_FALSE, ToFloat16(world).v);
             glUniform3f(u_color, 1.0f, 0.0f, 1.0f);
@@ -143,6 +144,7 @@ int main(void)
 
         // Hint: Use the RotateZ function
         case 5:
+            world = RotateZ(time);
             glUseProgram(shaderUniformColor);
             glUniformMatrix4fv(u_world, 1, GL_FALSE, ToFloat16(world).v);
             glUniform3f(u_color, 0.0f, 1.0f, 1.0f);
